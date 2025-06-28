7e032783404feba5dc0662032c76b17f2540af6e
vianovi <silviadewinovianaputri@email.com>
2025-06-28 20:03:49 +0700

    Testing awal

---
+++ Python_scripts.py
@@ -0,0 +1,8 @@
+# Impor modul yang diperlukan
+import json
+import os
+import datetime
+import time
+import hashlib # Untuk hashing password
+import uuid    # Untuk ID unik
+import re      # Untuk validasi input (misalnya email, format PIN)
